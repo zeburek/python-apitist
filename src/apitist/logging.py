6644f2ee1928ab93c6a4ccf3b4ac08013b8040a6
Parviz Khavari <p.khavari@omprussia.ru>
2019-08-23 09:57:41 +0300

    Remove stream handler from logger, use default in project

--- src/apitist/logging.py
+++ src/apitist/logging.py
@@ -1,5 +1,4 @@
 import logging
-import sys
 
 import apitist
 
@@ -21,9 +20,6 @@ class Logging:
         Args:
           log_level (int): minimum loglevel for emitting messages
         """
-        fmt = "[%(asctime)s][%(levelname)s][%(name)s]: %(message)s"
-        date_fmt = "%Y-%m-%d %H:%M:%S"
-        color_fmt = "%(log_color)s{}%(reset)s".format(fmt)
 
         # Suppress overly verbose logs from libraries that aren't helpful
         if log_level == logging.DEBUG:
@@ -33,30 +29,7 @@ class Logging:
             logging.getLogger("requests").setLevel(logging.WARNING)
             logging.getLogger("urllib3").setLevel(logging.WARNING)
 
-        try:
-            from colorlog import ColoredFormatter
-
-            logging.getLogger().handlers[0].setFormatter(
-                ColoredFormatter(
-                    color_fmt,
-                    datefmt=date_fmt,
-                    reset=True,
-                    log_colors={
-                        "DEBUG": "cyan",
-                        "INFO": "green",
-                        "WARNING": "yellow",
-                        "ERROR": "red",
-                        "CRITICAL": "red",
-                    },
-                )
-            )
-        except ImportError:
-            pass
-        fmt_ = logging.Formatter(fmt=fmt, datefmt=date_fmt)
-        stm = logging.StreamHandler()
-        stm.setFormatter(fmt_)
         Logging.logger = logging.getLogger(apitist.dist_name)
-        Logging.logger.addHandler(stm)
         Logging.logger.setLevel(log_level)
 
 
