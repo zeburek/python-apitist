51e2cdafa77d8ed9e5b276a5260c71d5e55c242d
Parviz Khavari <csctclan@gmail.com>
2020-11-08 22:11:30 +0300

    Add support for python 3.9
    Drop support for python 3.6

--- src/apitist/utils.py
+++ src/apitist/utils.py
@@ -1,16 +1,9 @@
 import sys
-from typing import (
-    Dict,
-    FrozenSet,
-    Mapping,
-    MutableSequence,
-    MutableSet,
-    Sequence,
-    Tuple,
-)
+from typing import List, MutableSequence, Sequence, Tuple
 
 version_info = sys.version_info[0:3]
-is_py37_or_higher = version_info[:2] in [(3, 7), (3, 8)]
+is_py37 = version_info[:2] == (3, 7)
+is_py38 = version_info[:2] == (3, 8)
 
 
 def is_attrs_class(cls):
@@ -26,8 +19,14 @@ def _subclass(typ):
     return lambda cls: issubclass(cls, typ)
 
 
-if is_py37_or_higher:
-    from typing import List, Union, _GenericAlias
+if is_py37 or is_py38:
+    from typing import Union, _GenericAlias
+
+    def is_tuple(type):
+        return type is Tuple or (
+            type.__class__ is _GenericAlias
+            and issubclass(type.__origin__, Tuple)
+        )
 
     def is_union_type(obj):
         return (
@@ -37,69 +36,44 @@ if is_py37_or_higher:
         )
 
     def is_sequence(type):
-        return (
-            type is List
-            or type.__class__ is _GenericAlias
+        return type is List or (
+            type.__class__ is _GenericAlias
+            and type.__origin__ is not Union
             and issubclass(type.__origin__, Sequence)
-        )
-
-    def is_frozenset(type):
-        return type.__class__ is _GenericAlias and issubclass(
-            type.__origin__, FrozenSet
-        )
-
-    def is_mutable_set(type):
-        return type.__class__ is _GenericAlias and issubclass(
-            type.__origin__, MutableSet
-        )
-
-    def is_tuple(type):
-        return type is Tuple or (
-            type.__class__ is _GenericAlias
-            and issubclass(type.__origin__, Tuple)
-        )
-
-    def is_mapping(type):
-        return type is Mapping or (
-            type.__class__ is _GenericAlias
-            and issubclass(type.__origin__, Mapping)
-        )
-
-    bare_list_args = List.__args__
-    bare_seq_args = Sequence.__args__
-    bare_mapping_args = Mapping.__args__
-    bare_dict_args = Dict.__args__
-    bare_mutable_seq_args = MutableSequence.__args__
-
-    def is_bare(type):
-        args = type.__args__
-        return (
-            args == bare_list_args
-            or args == bare_seq_args
-            or args == bare_mapping_args
-            or args == bare_dict_args
-            or args == bare_mutable_seq_args
         )
 
 
 else:
-    from typing import _Union
-
-    def is_union_type(obj):
-        """Return true if the object is a union. """
-        return isinstance(obj, _Union)
-
-    def is_frozenset(type):
-        return issubclass(type, FrozenSet)
-
-    def is_mutable_set(type):
-        return issubclass(type, MutableSet)
-
-    def is_sequence(type):
-        return issubclass(type, Sequence)
+    # 3.9+
+    from typing import (
+        Union,
+        _GenericAlias,
+        _UnionGenericAlias,
+    )
 
     def is_tuple(type):
-        return issubclass(type, Tuple)
+        return (
+            type in (Tuple, tuple)
+            or (
+                type.__class__ is _GenericAlias
+                and issubclass(type.__origin__, Tuple)
+            )
+            or (getattr(type, "__origin__", None) is tuple)
+        )
 
-    def is_mapping(type):
-        return issubclass(type, Mapping)
+    def is_union_type(obj):
+        return (
+            obj is Union
+            or isinstance(obj, _UnionGenericAlias)
+            and obj.__origin__ is Union
+        )
+
+    def is_sequence(type):
+        return (
+            type in (List, list, Sequence, MutableSequence)
+            or (
+                type.__class__ is _GenericAlias
+                and issubclass(type.__origin__, Sequence)
+            )
+            or (getattr(type, "__origin__", None) is list)
+        )
